f2615775a341fed0f16e3ea1d64d03bcf4c362a3
Sir Kodec <chukwumalume.onyeka@gmail.com>
2024-09-07 15:33:34 +0100

    credit score/user

--- user/UserData/profile.php
+++ user/UserData/profile.php
@@ -159,7 +159,7 @@ include("header.php");
                 <!-- assessment -->
                  
                 <div class="assessment">
-                <p>Credit Assessment</p>
+                <p>Credit Score</p>
                 <div class="score-container">
                     <div class="credit-bar-wrapper">
                         <div class="credit-bar" id="creditBar"></div>
